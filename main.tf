3523ae9cc486534398b242096bcea1db0c9386aa
oksuriini <oskari.peiponen@hotmail.com>
2024-03-06 10:43:00 +0200

    fixed again

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "aws_s3_bucket" "s3_bucket" {
 }
 
 resource "aws_s3_bucket_public_access_block" "s3_bucket" {
-  bucket = aws_s3_bucket.s3_bucket
+  bucket = aws_s3_bucket.s3_bucket.bucket
 
   block_public_acls       = true
   block_public_policy     = true
